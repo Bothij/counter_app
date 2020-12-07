17ba29ada96debb593acf7572d90aaf546ba4af0
Bothij <bothij@gmail.com>
2020-12-07 17:56:28 +0000

    communikations with user

--- main.cpp
+++ main.cpp
@@ -14,18 +14,53 @@ void PrintDivide(int number_1, int number_2);
 int main() {
   int number_1 = 20;
   int number_2 = 10;
-  
-  
-  AddTwoNumbers(number_1, number_2);
+  char option;
+
+  std::cout << "Választható műveletek:" << std::endl;
+  std::cout << "  a) Összeadás" << std::endl;
+  std::cout << "  b) Kivonás" << std::endl;
+  std::cout << "  c) Szorzás" << std::endl;
+  std::cout << "  b) Osztás" << std::endl;
+  std::cout << "Kérlek, válaszd ki a műveletet: " << std::endl;
+  std::cin >> option;
+
+  if (option == 'a') {
+    PrintAdd(number_1, number_2);
+  }
+  if (option == 'b') {
+    PrintSubtract(number_1, number_2);
+  }
+  if (option == 'c') {
+    PrintMultiply(number_1, number_2);
+  }
+  if (option == 'd') {
+    PrintDivide(number_1, number_2);
+  }
+
+// 1) Egy változó deklarálása a felhasználó által kiválasztott művelettípus lementéséhez.
+  // 2) Műveletek kilistázása a konzolra.
+  // 3) A művelet típusának bekérése a felhasználótól.
+  // 4) A két szám bekérése a felhasználótól.
+  // 5) A felhasználó által kiválasztott művelet végrehajtása az alábbiakból.
+  /*
+    a) Összeadás
+    b) Kivonás
+    c) Szorzás
+    d) Osztás
+  */
+  // 6) Helytelen bemenet esetén a program kiléptetése.
+}
+
+  /*AddTwoNumbers(number_1, number_2);
   SubtractTwoNumbers(number_1, number_2);
   MultiplyTwoNumbers(number_1, number_2);
   DivideTwoNumbers(number_1, number_2);
   PrintAdd(number_1, number_2);
   PrintSubtract(number_1, number_2);
   PrintMultiply(number_1, number_2);
-  PrintDivide(number_1, number_2);
+  PrintDivide(number_1, number_2);*/
   
-}
+
 
 //adding - összeadás
 int AddTwoNumbers(int number_1, int number_2) {
@@ -44,21 +79,17 @@ double DivideTwoNumbers(int number_1, int number_2) {
   }
   //print adding - összeadás kinyomtatása
 void PrintAdd(int number_1, int number_2) {
-  int adding = AddTwoNumbers(number_1, number_2);
-  std::cout << number_1  << " + " << number_2 << " = " << adding << std::endl;
+  std::cout << number_1  << " + " << number_2 << " = " << AddTwoNumbers(number_1, number_2) << std::endl;
   }
   //print subtraction - kivonás kinyomtatása
 void PrintSubtract(int number_1, int number_2) { 
-  int subtract = SubtractTwoNumbers(number_1, number_2);
-  std::cout << number_1  << " - " << number_2 << " = "    << subtract << std::endl;
+  std::cout << number_1  << " - " << number_2 << " = " << SubtractTwoNumbers(number_1, number_2) <<        std::endl;
   }
   //print multiplying - szorzás kinyomtatása
 void PrintMultiply(int number_1, int number_2) {
-  int multiply = MultiplyTwoNumbers(number_1, number_2);
-  std::cout << number_1  << " * " << number_2 << " = " << multiply << std::endl;
+  std::cout << number_1  << " * " << number_2 << " = " << MultiplyTwoNumbers(number_1, number_2) << std::endl;
   }
   //print division - osztás kinyomtatása
 void PrintDivide(int number_1, int number_2) {
-  double divide = DivideTwoNumbers(number_1, number_2);
-  std::cout << number_1  << " / " << number_2 << " = " << divide << std::endl;
+  std::cout << number_1  << " / " << number_2 << " = " << DivideTwoNumbers(number_1, number_2) << std::endl;
   }
