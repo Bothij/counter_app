8c95b0505152bd984e5f4921761222550c3945e1
Bothij <bothij@gmail.com>
2020-12-07 15:06:11 +0000

    convert to functions

--- main.cpp
+++ main.cpp
@@ -1,14 +1,36 @@
 #include <iostream>
 
+void AddTwoNumbers(int number_1, int number_2);
+void SubtractTwoNumbers(int number_1, int number_2);
+void MultiplyTwoNumbers(int number_1, int number_2);
+void DivideTwoNumber(int number_1, int number_2);
+
+
 int main() {
   int number_1 = 5;
   int number_2 = 10;
-  //adding - összeadás
-  std::cout << number_1  << " + " << number_2 << " = " << number_1 + number_2 << std::endl;
-  //subtraction - kivonás
-  std::cout << number_1  << " - " << number_2 << " = " << number_1 - number_2 << std::endl;
-  //multiplying - szorzás
-  std::cout << number_1  << " * " << number_2 << " = " << number_1 * number_2 << std::endl;
-  //division - osztás (typecasting)
-  std::cout << number_1  << " / " << number_2 << " = " << number_1 / (double)number_2 << std::endl;
+  
+  void AddTwoNumbers();
+  void SubtractTwoNumbers();
+  void MultiplyTwoNumbers();
+  void DivideTwoNumber();
+  
+  
+  
 }
+
+//adding - összeadás
+void AddTwoNumbers(int number_1, int number_2) {
+  std::cout << number_1  << " + " << number_2 << " = " << number_1 + number_2 << std::endl;
+  }
+  //subtraction - kivonás
+void SubtractTwoNumbers(int number_1, int number_2) {     std::cout << number_1  << " - " << number_2 << " = "    << number_1 - number_2 << std::endl;
+  }
+  //multiplying - szorzás
+void MultiplyTwoNumbers(int number_1, int number_2) {
+  std::cout << number_1  << " * " << number_2 << " = " << number_1 * number_2 << std::endl;
+  }
+  //division - osztás (typecasting)
+void DivideTwoNumbers(int number_1, int number_2) {
+  std::cout << number_1  << " / " << number_2 << " = " << number_1 / (double)number_2 << std::endl;
+  }
