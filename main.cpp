e036d80d1ee45485d343481dc83138540d40f64d
Bothij <bothij@gmail.com>
2020-12-07 16:15:42 +0000

    separation of concerns

--- main.cpp
+++ main.cpp
@@ -1,36 +1,64 @@
 #include <iostream>
 
-void AddTwoNumbers(int number_1, int number_2);
-void SubtractTwoNumbers(int number_1, int number_2);
-void MultiplyTwoNumbers(int number_1, int number_2);
-void DivideTwoNumbers(int number_1, int number_2);
+int AddTwoNumbers(int number_1, int number_2);
+int SubtractTwoNumbers(int number_1, int number_2);
+int MultiplyTwoNumbers(int number_1, int number_2);
+double DivideTwoNumbers(int number_1, int number_2);
+void PrintAdd(int number_1, int number_2);
+void PrintSubtract(int number_1, int number_2);
+void PrintMultiply(int number_1, int number_2);
+void PrintDivide(int number_1, int number_2);
+
 
 
 int main() {
-  int number_1 = 5;
+  int number_1 = 20;
   int number_2 = 10;
+  
   
   AddTwoNumbers(number_1, number_2);
   SubtractTwoNumbers(number_1, number_2);
   MultiplyTwoNumbers(number_1, number_2);
   DivideTwoNumbers(number_1, number_2);
-  
-  
+  PrintAdd(number_1, number_2);
+  PrintSubtract(number_1, number_2);
+  PrintMultiply(number_1, number_2);
+  PrintDivide(number_1, number_2);
   
 }
 
 //adding - összeadás
-void AddTwoNumbers(int number_1, int number_2) {
-  std::cout << number_1  << " + " << number_2 << " = " << number_1 + number_2 << std::endl;
-  }
+int AddTwoNumbers(int number_1, int number_2) {
+  return number_1 + number_2;
+    }
   //subtraction - kivonás
-void SubtractTwoNumbers(int number_1, int number_2) {     std::cout << number_1  << " - " << number_2 << " = "    << number_1 - number_2 << std::endl;
+int SubtractTwoNumbers(int number_1, int number_2) {     return number_1 - number_2;
   }
   //multiplying - szorzás
-void MultiplyTwoNumbers(int number_1, int number_2) {
-  std::cout << number_1  << " * " << number_2 << " = " << number_1 * number_2 << std::endl;
+int MultiplyTwoNumbers(int number_1, int number_2) {
+  return number_1 * number_2;
   }
   //division - osztás (typecasting)
-void DivideTwoNumbers(int number_1, int number_2) {
-  std::cout << number_1  << " / " << number_2 << " = " << number_1 / (double)number_2 << std::endl;
+double DivideTwoNumbers(int number_1, int number_2) {
+  return number_1 / (double)number_2;
+  }
+  //print adding - összeadás kinyomtatása
+void PrintAdd(int number_1, int number_2) {
+  int adding = AddTwoNumbers(number_1, number_2);
+  std::cout << number_1  << " + " << number_2 << " = " << adding << std::endl;
+  }
+  //print subtraction - kivonás kinyomtatása
+void PrintSubtract(int number_1, int number_2) { 
+  int subtract = SubtractTwoNumbers(number_1, number_2);
+  std::cout << number_1  << " - " << number_2 << " = "    << subtract << std::endl;
+  }
+  //print multiplying - szorzás kinyomtatása
+void PrintMultiply(int number_1, int number_2) {
+  int multiply = MultiplyTwoNumbers(number_1, number_2);
+  std::cout << number_1  << " * " << number_2 << " = " << multiply << std::endl;
+  }
+  //print division - osztás kinyomtatása
+void PrintDivide(int number_1, int number_2) {
+  double divide = DivideTwoNumbers(number_1, number_2);
+  std::cout << number_1  << " / " << number_2 << " = " << divide << std::endl;
   }
