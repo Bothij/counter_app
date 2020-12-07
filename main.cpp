b7970020ba9e816dd6b73e283045f5ca8ae42bff
Bothij <bothij@gmail.com>
2020-12-07 14:22:27 +0000

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+
+int main() {
+  int number_1 = 5;
+  int number_2 = 10;
+  //adding - összeadás
+  std::cout << number_1  << " + " << number_2 << " = " << number_1 + number_2 << std::endl;
+  //subtraction - kivonás
+  std::cout << number_1  << " - " << number_2 << " = " << number_1 - number_2 << std::endl;
+  //multiplying - szorzás
+  std::cout << number_1  << " * " << number_2 << " = " << number_1 * number_2 << std::endl;
+}
