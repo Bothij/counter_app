b25ca698adb1ba71ce7bc383ce8c68b6e28bc3e1
Bothij <bothij@gmail.com>
2020-12-08 19:22:03 +0000

    math.h library use

--- main.cpp
+++ main.cpp
@@ -1,13 +1,16 @@
 #include <iostream>
+#include <math.h>
 
 int AddTwoNumbers(int number_1, int number_2);
 int SubtractTwoNumbers(int number_1, int number_2);
 int MultiplyTwoNumbers(int number_1, int number_2);
 double DivideTwoNumbers(int number_1, int number_2);
+int ExponentiateTwoNumbers(int number_1, int number_2);
 void PrintAdd(int number_1, int number_2);
 void PrintSubtract(int number_1, int number_2);
 void PrintMultiply(int number_1, int number_2);
 void PrintDivide(int number_1, int number_2);
+void PrintExponent(int number_1, int number_2);
 void ListOperations();
 void CalculateResult(char option, int number_1, int number_2);
 
@@ -24,7 +27,7 @@ int main() {
       std::cout << "Viszlát!" << std::endl;
      continue;
       }
-      else if(option != 'a' && option != 'b' && option != 'c'   && option != 'd') {
+      else if(option != 'a' && option != 'b' && option != 'c' && option != 'd' && option != 'e') {
       std::cerr << "Rossz karaktert adtál meg, ezért a program leáll!" << std::endl;
       return 1;
       }
@@ -45,16 +48,19 @@ int main() {
   
 int AddTwoNumbers(int number_1, int number_2) {
   return number_1 + number_2;
-  }
+}
 int SubtractTwoNumbers(int number_1, int number_2) {     return number_1 - number_2;
-  }
+}
 int MultiplyTwoNumbers(int number_1, int number_2) {
   return number_1 * number_2;
-  }
+}
 double DivideTwoNumbers(int number_1, int number_2) {
   return number_1 / (double)number_2;
-  }
- 
+}
+int ExponentiateTwoNumbers(int number_1, int number_2){
+  return pow(number_1, number_2);
+}
+
 void PrintAdd(int number_1, int number_2) {
   std::cout << number_1  << " + " << number_2 << " = " << AddTwoNumbers(number_1, number_2) << std::endl;
   }
@@ -71,13 +77,18 @@ void PrintDivide(int number_1, int number_2) {
   std::cout << number_1  << " / " << number_2 << " = " << DivideTwoNumbers(number_1, number_2) << std::endl;
   }
 
+void PrintExponent(int number_1, int number_2) {
+  std::cout << pow(number_1, number_2) << std::endl;
+}
+
 void ListOperations() {
   std::cout << "Választható műveletek: " << std::endl;
-  std::cout << "\ta) Összeadás" << std::endl;
-  std::cout << "\tb) Kivonás" << std::endl;
-  std::cout << "\tc) Szorzás" << std::endl;
-  std::cout << "\td) Osztás" << std::endl;
-  std::cout << "\tx) Kilépés" << std::endl;
+  std::cout << "\ta) - Összeadás" << std::endl;
+  std::cout << "\tb) - Kivonás" << std::endl;
+  std::cout << "\tc) - Szorzás" << std::endl;
+  std::cout << "\td) - Osztás" << std::endl;
+  std::cout << "\te) - Hatványozás" << std::endl;
+  std::cout << "\tx) - Kilépés" << std::endl;
   std::cout << "Művelet kiválasztásához üsd be a megfelelő betűt!" << std::endl;
 }
 void CalculateResult(char option, int number_1, int number_2) {
@@ -89,6 +100,8 @@ void CalculateResult(char option, int number_1, int number_2) {
     PrintMultiply(number_1, number_2);
   } else if (option == 'd') {
     PrintDivide(number_1, number_2);
+  } else if (option == 'e') {
+    PrintExponent(number_1, number_2);
   }
 }
 
